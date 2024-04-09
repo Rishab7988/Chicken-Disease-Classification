47bbe699389da64bf6891cc0c7a9533176e9f473
Rishab Mahato <rishabmahato2003@gmail.com>
2024-04-09 21:08:00 +0530

    added logger and logs

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import logging
+
+
+# specifying the log format
+# ascii time, levelname(info,debug,critical,info,), module(name of python file), message corr. to it
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+# creating a directory to store the logs
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout) # using StreamHandler we can print the logs on the terminal 
+    ]
+)
+
+logger = logging.getLogger("cnnClassifierLogger")
+
+logger.info("Welcome to my custom logger")
